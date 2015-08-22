045fbc4ed0c3e0dbb2afe37b99697333d374933a
Yumen Cao <gawain@yumentsao.com>
2015-08-22 13:48:35 -0500

    init project

---
+++ Ten/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ten
+//
+//  Created by Yumen Cao on 8/22/15.
+//  Copyright (c) 2015 LiMao Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
