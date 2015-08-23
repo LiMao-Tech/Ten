b05fff49e0f6f2671fd6f6d41d4e66226824b9f9
YifangZhang <yifangzhang2009@gmail.com>
2015-08-23 17:02:34 -0500

    Authorization of location manager is completed, now user will be request if they did not authorize for the location service.

--- Ten/AppDelegate.swift
+++ Ten/AppDelegate.swift
@@ -16,6 +16,8 @@ let SCREEN_WIDTH = UIScreen.mainScreen().bounds.width
 
 let DISTANCE_FILTER : Double = 100 // meters
 
+public var AUTHORIZATION_STATUS_FLAG : Int = 0
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
     var window: UIWindow?
@@ -35,19 +37,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         if CLLocationManager.authorizationStatus() == .NotDetermined {
             locationManager.requestWhenInUseAuthorization()
         }
+        
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         locationManager.distanceFilter = DISTANCE_FILTER
         
-        let status = CLLocationManager.authorizationStatus()
-        switch CLLocationManager.authorizationStatus() {
-        case .AuthorizedAlways:
+        
+        
+            
+        return true
+    }
+    
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        // going to be something:
+        switch status {
+        
+        case .AuthorizedWhenInUse:
+            AUTHORIZATION_STATUS_FLAG = 1
             break
-            
-        case .NotDetermined:
-            locationManager.requestWhenInUseAuthorization()
-            
-        case .AuthorizedWhenInUse, .Restricted, .Denied:
+        case .Denied, .NotDetermined, .Restricted:
             let alertController = UIAlertController(
                 title: "Background Location Access Disabled",
                 message: "In order to be notified about adorable kittens near you, please open this app's settings and set location access to 'Always'.",
@@ -62,31 +70,81 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
                 }
             }
             alertController.addAction(openAction)
-        }
             
-        return true
+            self.window!.rootViewController?.presentViewController(alertController, animated: true, completion: nil)
+            
+            break
+        default:
+            AUTHORIZATION_STATUS_FLAG = -1 // error
+        }
     }
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        println("in Will Resign Active")
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        println("in Did Enter Background State")
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        println("in Will Enter Foreground State")
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        println("*********** in Did Become Active *************")
+        
+        // checking for location Manager's authorization status
+        
+        let status = CLLocationManager.authorizationStatus()
+        println("status is: \(status.rawValue)")
+        switch status {
+        case .AuthorizedWhenInUse:
+            AUTHORIZATION_STATUS_FLAG = 1 // passed
+            break
+            
+        case .NotDetermined, .Restricted, .Denied:
+            
+            println("<<<< --- in denied case --- >>>>")
+            let alertController = UIAlertController(
+                title: "Background Location Access Disabled",
+                message: "In order to be notified about adorable kittens near you, please open this app's settings and set location access to 'Always'.",
+                preferredStyle: .Alert)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+            alertController.addAction(cancelAction)
+            
+            let openAction = UIAlertAction(title: "Open Settings", style: .Default) { (action) in
+                if let url = NSURL(string:UIApplicationOpenSettingsURLString) {
+                    UIApplication.sharedApplication().openURL(url)
+                }
+            }
+            alertController.addAction(openAction)
+            
+            self.window!.rootViewController?.presentViewController(alertController, animated: true, completion: nil)
+
+            // keep authorization status flag to be 0;
+            break
+            
+        default:
+            // error state
+            AUTHORIZATION_STATUS_FLAG = -1
+            break
+        }
+        
+        // end of location manager authorization
+        
     }
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        println("in Will Terminate State, byebye")
     }
 
 
