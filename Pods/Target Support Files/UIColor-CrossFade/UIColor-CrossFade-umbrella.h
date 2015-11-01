3ff0c1abcebf41f17eaa82a64e57a34e84070884
Yumen Cao <yumentsao@Yumens-MacBook-Pro.local>
2015-11-01 15:10:18 -0600

    Trivial

---
+++ Pods/Target Support Files/UIColor-CrossFade/UIColor-CrossFade-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "UIColor+CrossFade.h"
+
+FOUNDATION_EXPORT double UIColor_CrossFadeVersionNumber;
+FOUNDATION_EXPORT const unsigned char UIColor_CrossFadeVersionString[];
+
