81f60fa3f28e281f47733f8200712d55d24fc957
Yumen Cao <yumentsao@Yumens-MacBook-Pro.local>
2015-11-04 00:24:06 -0600

    removed pods

--- Pods/Target Support Files/UIColor-CrossFade/UIColor-CrossFade-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_UIColor_CrossFade : NSObject
-@end
-@implementation PodsDummy_UIColor_CrossFade
-@end
--- Pods/Target Support Files/UIColor-CrossFade/UIColor-CrossFade-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "UIColor+CrossFade.h"
-
-FOUNDATION_EXPORT double UIColor_CrossFadeVersionNumber;
-FOUNDATION_EXPORT const unsigned char UIColor_CrossFadeVersionString[];
-
